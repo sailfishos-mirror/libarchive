sailfishos-mirror/libarchive

commit 16ad9310733e4d73ed55504e57ff2919f707a1c2

cpio reader: Validate pathname in record_hardlink
Prevent null pointer dereference and heap-use-after-free when
processing CPIO archives with hardlink entries that lack pathname
information.

libarchive/archive_read_support_format_cpio.c
@@ -1102,6 +1102,16 @@ record_hardlink(struct archive_read *a,
 		    ENOMEM, "Out of memory adding file to list");
 		return (ARCHIVE_FATAL);
 	}
+
+	const char *pathname = archive_entry_pathname(entry);
+	if (pathname == NULL) {
+		archive_set_error(&a->archive,
+			ARCHIVE_ERRNO_FILE_FORMAT,
+           "Invalid hardlink entry with no pathname");
+		free(le);
+		return (ARCHIVE_FATAL);
+	}
+
 	if (cpio->links_head != NULL)
 		cpio->links_head->previous = le;
 	le->next = cpio->links_head;
@@ -1110,10 +1120,11 @@ record_hardlink(struct archive_read *a,
 	le->dev = dev;
 	le->ino = ino;
 	le->links = archive_entry_nlink(entry) - 1;
-	le->name = strdup(archive_entry_pathname(entry));
+	le->name = strdup(pathname);
 	if (le->name == NULL) {
 		archive_set_error(&a->archive,
 		    ENOMEM, "Out of memory adding file to list");
+		free(le);
 		return (ARCHIVE_FATAL);
 	}
 

commit 78846739ce405f80460b8d4cc5b1dcb772f76610

Merge pull request #3055 from stoeckmann/cpio_uaf
cpio: Fix UAF in error path
Add entry only after its full initialization into list. Otherwise the error handling of a failing strdup would have to unlink the entry again.

Fixes: 16ad9310733e ("cpio reader: Validate pathname in record_hardlink")

Resolves #3053.

(cherry picked from commit c03931678bab0d215f4ea589eb59b9e2cc81cdc5)

libarchive/archive_read_support_format_cpio.c
@@ -1122,11 +1122,6 @@ record_hardlink(struct archive_read *a,
 		return (ARCHIVE_FATAL);
 	}
 
-	if (cpio->links_head != NULL)
-		cpio->links_head->previous = le;
-	le->next = cpio->links_head;
-	le->previous = NULL;
-	cpio->links_head = le;
 	le->dev = dev;
 	le->ino = ino;
 	le->links = archive_entry_nlink(entry) - 1;
@@ -1137,6 +1132,12 @@ record_hardlink(struct archive_read *a,
 		free(le);
 		return (ARCHIVE_FATAL);
 	}
+
+	if (cpio->links_head != NULL)
+		cpio->links_head->previous = le;
+	le->next = cpio->links_head;
+	le->previous = NULL;
+	cpio->links_head = le;
 
 	return (ARCHIVE_OK);
 }